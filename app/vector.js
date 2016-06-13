033c5624e54cd5ae753f20b64781df2f161a49bc
geomodular <geomodular@gmail.com>
2016-06-13 23:09:42 +0200

    Added small vector function

---
+++ app/vector.js
@@ -0,0 +1,12 @@
+
+/* Immutable type */
+export default function Vector(x1, x2) {
+  return {
+    length: () => Math.sqrt(x1 * x1 + x2 * x2),
+    rotate(phi) {
+      const c = Math.cos(phi),
+            s = Math.sin(phi);
+      return Vector(c * x1 - s * x2, s * x1 + c * x2);
+    }
+  };
+}
